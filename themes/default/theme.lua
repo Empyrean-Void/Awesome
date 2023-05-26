56576429c8bcbe9a60b02150e86304dfdff14391
Empyrean-Void <jshortz@proton.me>
2023-05-26 17:33:30 -0600

    Added color to titlebar buttons for active client

--- themes/default/theme.lua
+++ themes/default/theme.lua
@@ -2,6 +2,7 @@
 -- Default awesome theme --
 ---------------------------
 
+local gears = require("gears")
 local theme_assets = require("beautiful.theme_assets")
 local xresources = require("beautiful.xresources")
 local dpi = xresources.apply_dpi
@@ -32,6 +33,7 @@ theme.border_focus  = "#282828"
 theme.border_marked = "#fb4934"
 
 theme.accent = "#8fa598"
+theme.green = "#b8bb26"
 
 -- There are other variable sets
 -- overriding the default one when
@@ -74,31 +76,13 @@ theme.menu_width  = dpi(100)
 --theme.bg_widget = "#cc0000"
 
 -- Define the image to load
-theme.titlebar_close_button_normal = themes_path.."default/titlebar/close_normal.png"
-theme.titlebar_close_button_focus  = themes_path.."default/titlebar/close_focus.png"
+theme.titlebar_close_button_normal = gears.color.recolor_image(themes_path.. "default/titlebar/close_normal.png", theme.fg_normal)
+theme.titlebar_close_button_focus  = gears.color.recolor_image(themes_path.. "default/titlebar/close_normal.png", theme.bg_urgent)
 
-theme.titlebar_minimize_button_normal = themes_path.."default/titlebar/minimize_normal.png"
-theme.titlebar_minimize_button_focus  = themes_path.."default/titlebar/minimize_focus.png"
-
-theme.titlebar_ontop_button_normal_inactive = themes_path.."default/titlebar/ontop_normal_inactive.png"
-theme.titlebar_ontop_button_focus_inactive  = themes_path.."default/titlebar/ontop_focus_inactive.png"
-theme.titlebar_ontop_button_normal_active = themes_path.."default/titlebar/ontop_normal_active.png"
-theme.titlebar_ontop_button_focus_active  = themes_path.."default/titlebar/ontop_focus_active.png"
-
-theme.titlebar_sticky_button_normal_inactive = themes_path.."default/titlebar/sticky_normal_inactive.png"
-theme.titlebar_sticky_button_focus_inactive  = themes_path.."default/titlebar/sticky_focus_inactive.png"
-theme.titlebar_sticky_button_normal_active = themes_path.."default/titlebar/sticky_normal_active.png"
-theme.titlebar_sticky_button_focus_active  = themes_path.."default/titlebar/sticky_focus_active.png"
-
-theme.titlebar_floating_button_normal_inactive = themes_path.."default/titlebar/floating_normal_inactive.png"
-theme.titlebar_floating_button_focus_inactive  = themes_path.."default/titlebar/floating_focus_inactive.png"
-theme.titlebar_floating_button_normal_active = themes_path.."default/titlebar/floating_normal_active.png"
-theme.titlebar_floating_button_focus_active  = themes_path.."default/titlebar/floating_focus_active.png"
-
-theme.titlebar_maximized_button_normal_inactive = themes_path.."default/titlebar/maximized_normal_inactive.png"
-theme.titlebar_maximized_button_focus_inactive  = themes_path.."default/titlebar/maximized_focus_inactive.png"
-theme.titlebar_maximized_button_normal_active = themes_path.."default/titlebar/maximized_normal_active.png"
-theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/maximized_focus_active.png"
+theme.titlebar_maximized_button_normal_inactive = gears.color.recolor_image(themes_path.. "default/titlebar/maximized_normal_inactive.png", theme.fg_normal)
+theme.titlebar_maximized_button_focus_inactive  = gears.color.recolor_image(themes_path.. "default/titlebar/maximized_normal_inactive.png", theme.green)
+theme.titlebar_maximized_button_normal_active = gears.color.recolor_image(themes_path.. "default/titlebar/maximized_normal_active.png", theme.fg_normal)
+theme.titlebar_maximized_button_focus_active  = gears.color.recolor_image(themes_path.. "default/titlebar/maximized_normal_active.png", theme.green)
 
 -- theme.wallpaper = themes_path.."default/gruvbox.jpg"
 
