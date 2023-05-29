241be573273fe9837dfc163d1798ae6bf7aebee7
Empyrean-Void <jshortz@proton.me>
2023-05-29 17:43:23 -0600

    Updated install script

--- install.sh
+++ install.sh
@@ -30,9 +30,37 @@ sudo pacman -S --needed firefox neovim
 echo ""
 echo "Creating backup of old configuration files..."
 
+echo "Awesome Window Manager..."
 if [ -d ~/.config/awesome ]; then
     mv ~/.config/awesome ~/.config/awesome.bak
     echo "Backed up ~/.config/awesome to ~/.config/awesome.bak"
+
+else
+    echo "Configuration files not found, skipping..."
+fi
+
+echo "Neovim..."
+if [ -d ~/.config/nvim ]; then
+    mv ~/.config/nvim ~/.config/nvim.bak
+    echo "Backed up ~/.config/nvim to ~/.config/nvim.bak"
+
+else
+    echo "Configuration files not found, skipping..."
+fi
+
+echo "Picom..."
+if [ -d ~/.config/picom ]; then
+    mv ~/.config/picom ~/.config/picom.bak
+    echo "Backed up ~/.config/picom to ~/.config/picom.bak"
+
+else
+    echo "Configuration files not found, skipping..."
+fi
+
+echo "Rofi..."
+if [ -d ~/.config/rofi ]; then
+    mv ~/.config/rofi ~/.config/rofi.bak
+    echo "Backed up ~/.config/rofi to ~/.config/rofi.bak"
 
 else
     echo "Configuration files not found, skipping..."
@@ -42,6 +70,12 @@ echo ""
 echo "Downloading configuration files..."
 
 git clone https://github.com/Empyrean-Void/Awesome.git ~/.config/awesome/
+
+git clone https://github.com/Empyrean-Void/Neovim.git ~/.config/nvim/
+
+git clone https://github.com/Empyrean-Void/Picom.git ~/.config/picom/
+
+git clone https://github.com/Empyrean-Void/Rofi.git ~/.config/rofi/
 
 echo ""
 echo "Installation finished!"
